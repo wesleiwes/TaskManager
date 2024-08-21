c84124a8002f3380946bc0bcd0a12e7829473b42
Weslei Rodrigues Alves <wesleiralves@gmail.com>
2024-08-21 15:35:34 -0300

    Implementada a classe TarefaRepositorio e UsuarioRepositorio para fornecer a lógica de acesso ao banco de dados para as operações.

--- TaskManager.Infraestrutura/Repositorio/TarefaRepositorio.cs
+++ TaskManager.Infraestrutura/Repositorio/TarefaRepositorio.cs
@@ -1,6 +1,56 @@
-﻿namespace TaskManager.Infraestrutura.Repositorio;
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Entidades;
+using TaskManager.Domain.Enuns;
+using TaskManager.Infraestrutura.Interface;
 
-internal class TarefaRepositorio
+namespace TaskManager.Infraestrutura.Repositorio;
+
+internal class TarefaRepositorio(AppDbContext context) : ITarefaRepositorio
 {
+
+    private readonly AppDbContext _context = context;
+
+    public async Task<Tarefa> CrieTarefa(Tarefa tarefa)
+    {
+        _context.Tarefas.Add(tarefa);
+        await _context.SaveChangesAsync();
+        return tarefa;
+    }
+    public async Task<bool> Atualizar(Tarefa tarefa)
+    {
+        _context.Tarefas.Update(tarefa);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<Tarefa?> ObterPorId(int id) =>
+        await _context.Tarefas.FindAsync(id);
+
+    public async Task<IEnumerable<Tarefa>> ObterTarefasPorDataDeCriacao(DateTime data) =>
+        await _context.Tarefas.Where(t => t.DataCriacao == data).ToListAsync();
+
+    public async Task<IEnumerable<Tarefa>> ObterTarefasPorDataDeConclusao(DateTime data) =>
+        await _context.Tarefas.Where(t => t.DataConclusao == data).ToListAsync();
+
+    public async Task<IEnumerable<Tarefa>> ObterTarefasPorStatus(StatusTarefa status) =>
+        await _context.Tarefas.Where(t => t.Status == status).ToListAsync();
+
+    public async Task<IEnumerable<Tarefa>> ObterTodasTarefas() =>
+        await _context.Tarefas.ToListAsync();
+
+    public async Task<IEnumerable<Tarefa>> ObterTodasTarefasPorUsuario(int idUsuario) =>
+        await _context.Tarefas.Where(t => t.UsuarioId == idUsuario).ToListAsync();
+
+
+    public async Task<bool> Remover(int id)
+    {
+        var tarefa = ObterPorId(id);
+
+        _context.Remove(tarefa);
+
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
 }
 
--- TaskManager.Infraestrutura/Repositorio/UsuarioRepositorio.cs
+++ TaskManager.Infraestrutura/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,40 @@
-﻿namespace TaskManager.Infraestrutura.Repositorio;
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Entidades;
+using TaskManager.Infraestrutura.Interface;
 
-internal class UsuarioRepositorio
+namespace TaskManager.Infraestrutura.Repositorio;
+
+public class UsuarioRepositorio(AppDbContext context) : IUsuarioRepositorio
 {
+    private readonly AppDbContext _context = context;
+
+    public async Task Adicionar(Usuario usuario)
+    {
+        _context.Add(usuario);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task Atualizar(Usuario usuario)
+    {
+        _context.Update(usuario);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Usuario?> ObterPorId(int id) =>
+        await _context.Usuarios.FindAsync(id);
+
+    public async Task<IEnumerable<Usuario>> ObterTodos() =>
+        await _context.Usuarios.ToListAsync();
+
+    public async Task Remover(int id)
+    {
+        Usuario? usuario = await ObterPorId(id);
+
+        if (usuario != null)
+        {
+            _context.Usuarios.Remove(usuario);
+            await _context.SaveChangesAsync();
+        }
+    }
 }
 
