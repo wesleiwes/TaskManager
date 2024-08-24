145d0cd140569cd24d01a6df9b9360c3b0d18a08
Weslei Rodrigues Alves <wesleiralves@gmail.com>
2024-08-24 19:36:16 -0300

    Implementação dos mapeadores das entiidades(Domain) com as Dtos

---
+++ TaskManager.Services/Mapeadores/MapeadorDeEntidades.cs
@@ -0,0 +1,62 @@
+﻿using TaskManager.Aplicacao.DTOs;
+using TaskManager.Domain.Entidades;
+
+namespace TaskManager.Aplicacao.Mapeadores;
+
+public static class MapeadorDeEntidades
+{
+    public static UsuarioDto ConvertaUsuarioParaDto(Usuario usuario)
+    {
+        return new UsuarioDto
+        {
+            IdUsuario = usuario.IdUsuario,
+            NomeCompleto = usuario.NomeCompleto,
+            Email = usuario.Email,
+            Login = usuario.Login,
+            Tarefas = usuario.Tarefas.Select(t => ConvertaTarefaParaDto(t)).ToList()
+        };
+    }
+
+    public static TarefaDto ConvertaTarefaParaDto(Tarefa tarefa)
+    {
+        return new TarefaDto
+        {
+            IdTarefa = tarefa.IdTarefa,
+            Nome = tarefa.Nome,
+            Descricao = tarefa.Descricao,
+            DataCriacao = tarefa.DataCriacao,
+            DataConclusao = tarefa.DataConclusao,
+            Status = tarefa.Status,
+            Prioridade = tarefa.Prioridade,
+            UsuarioId = tarefa.UsuarioId
+        };
+    }
+
+    public static Tarefa ConvertaDtoParaTarefa(TarefaDto tarefa)
+    {
+        return new Tarefa
+        {
+            IdTarefa = tarefa.IdTarefa,
+            Nome = tarefa.Nome,
+            Descricao = tarefa.Descricao,
+            DataCriacao = tarefa.DataCriacao,
+            DataConclusao = tarefa.DataConclusao,
+            Status = tarefa.Status,
+            Prioridade = tarefa.Prioridade,
+            UsuarioId = tarefa.UsuarioId
+        };
+    }
+
+    public static Usuario ConvertaDtoParaUsuario(UsuarioDto usuario)
+    {
+        return new Usuario
+        {
+            IdUsuario = usuario.IdUsuario,
+            NomeCompleto = usuario.NomeCompleto,
+            Email = usuario.Email,
+            Login = usuario.Login,
+            Tarefas = usuario.Tarefas.Select(t => ConvertaDtoParaTarefa(t)).ToList()
+        };
+    }
+}
+
