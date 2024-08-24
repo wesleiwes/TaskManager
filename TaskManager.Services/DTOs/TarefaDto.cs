ac6f7d2e0e464db3d3b10e6ad8027da53681f7fe
Weslei Rodrigues Alves <wesleiralves@gmail.com>
2024-08-24 19:35:19 -0300

    Implementação das Dtos

---
+++ TaskManager.Services/DTOs/TarefaDto.cs
@@ -0,0 +1,25 @@
+﻿using TaskManager.Domain.Enuns;
+
+namespace TaskManager.Aplicacao.DTOs;
+
+public class TarefaDto
+{
+    public int IdTarefa { get; set; }
+
+    public string Nome { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public DateTime DataCriacao { get; set; }
+
+    public DateTime? DataConclusao { get; set; }
+
+    public StatusTarefa Status { get; set; }
+
+    public PrioridadeTarefa Prioridade { get; set; }
+
+    public int UsuarioId { get; set; }
+
+    public UsuarioDto Usuario { get; set; }
+}
+
---
+++ TaskManager.Services/DTOs/UsuarioDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManager.Aplicacao.DTOs;
+
+public class UsuarioDto
+{
+    public int IdUsuario { get; set; }
+
+    public string Login { get; set; }
+
+    public string? Email { get; set; }
+
+    public string NomeCompleto { get; set; }
+
+    public IEnumerable<TarefaDto> Tarefas { get; set; } = [];
+}
+
